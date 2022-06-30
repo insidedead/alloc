bb7dc7139772f7113389baf2eaa46d59b558505d
insidedead <nhocosousa@outlook.com>
2022-06-30 21:09:40 +0200

    alloc

--- allocator.cpp
+++ allocator.cpp
@@ -1,47 +1,46 @@
 #include <cstddef>
 #include <cstdlib>
-
-template <class T>
-class Allocator {
-	static std::size_t l;
-	public:
-	static inline void allocate(const std::size_t);
-	static T *data;
-	static inline void construct(T);
-	static T test(unsigned);
-};
-
-template <class T>
-Allocator<T>::data{ nullptr };
-
-template <class T>
-void Allocator<T>::allocate(const std::size_t size)
-{
-	data = (T*) calloc(size, sizeof(T));
-}
-
-template <class T>
-void Allocator<T>::construct(T val)
-{
-	data[l++] = val;
-}
-
 #include <iostream>
 
-template <class T>
-T Allocator<T>::test(unsigned i)
+template <typename T>
+class Allocator {
+	static T* data;
+	static unsigned track;
+	public:
+	static T* allocate(std::size_t);
+	static inline void construct(T);
+	static inline void print(std::size_t);
+};
+
+template <typename T>
+T* Allocator<T>::data { nullptr };
+
+template <typename T>
+unsigned Allocator<T>::track { 0 };
+
+template <typename T>
+T* Allocator<T>::allocate(std::size_t size)
 {
-	return data[i];
+	data = (T*) calloc(size, sizeof(T));
+
+	return data;
 }
 
-template <class T>
-std::size_t Allocator<T>::l{ 0 };
+template <typename T>
+void Allocator<T>::construct(T v)
+{
+	data[track++] = v;
+}
+
+template <typename T>
+void Allocator<T>::print(std::size_t index)
+{
+	std::cout << data[index];
+}
 
 int main()
 {
-	Allocator<int>::allocate(5);
+	int *i = Allocator<int>::allocate(5);
 	Allocator<int>::construct(69);
-	std::cout << Allocator<int>::test(0);
-
-	return 0;
+	Allocator<int>::print(0);
 }
